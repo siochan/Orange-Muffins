39dacb355aa67079f5a9c7f88f107ee08c8364ad
Luis <lfluna@uci.edu>
2018-07-15 12:03:20 -0700

    Icecicle mechanics and sprites

---
+++ VGDCPlatformer/Assets/scripts/Enemies/DamageOnTouch.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DamageOnTouch : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+    public void OnTriggerEnter2D(Collider2D collision)
+    {
+
+        if (collision.gameObject.tag == "hurtbox")
+        {
+            //to kill enemy, we tell the enemy script
+            TheEnemy script = collision.gameObject.GetComponentInParent<TheEnemy>();
+            script.Die();
+        }
+    }
+}
---
+++ VGDCPlatformer/Assets/scripts/Enemies/TriggerFall.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TriggerFall : MonoBehaviour {
+
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.CompareTag("Player"))
+        {
+            Debug.Log("hi");
+            Rigidbody2D rb= gameObject.GetComponentInChildren<Rigidbody2D>();
+            rb.isKinematic = false;
+            Destroy(gameObject, 2f);
+        }
+    }
+}
