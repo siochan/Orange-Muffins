3b5953f3d9d0b0b3c2713543268cde9f5e782399
Ittai Mann <ittaiam@gmail.com>
2018-07-15 17:11:58 -0700

    Sloped movement no jumps

--- VGDCPlatformer/Assets/scripts/Player/NewPlayerMOvement.cs
+++ VGDCPlatformer/Assets/scripts/Player/NewPlayerMOvement.cs
@@ -7,64 +7,115 @@ public class NewPlayerMOvement : MonoBehaviour {
 
     private Rigidbody2D rb;
 
-    private ContactPoint2D[] contacts = new ContactPoint2D[5];
-    private ContactPoint2D CurrentCollider;
 	public Vector2 perp;
 	public float speed;
 	public float grav;
-	public float falling;
+	public bool falling;
 	public bool grounded;
+	public bool jumping;
+
+	public float groundcheckVector;
+	public float TeleportCheckVector;
+    public LayerMask groundLayer;
+
+	private CircleCollider2D ColliderAttach;
+
     // Use this for initialization
     void Awake () {
         rb = GetComponent<Rigidbody2D>();
 		grav= Physics2D.gravity.y;
-
+		ColliderAttach= GetComponent<CircleCollider2D>();
     }
 	
 	
 	void Update()
 	{
-		//if(rb.GetContacts)
+        if (Input.GetButtonDown("Jump") && grounded)
+        {
+        
+            rb.velocity = Vector2.up * 10;
+			grounded=false;
+			jumping=true;
+		}
 	}
 
 	void FixedUpdate()
-	{	
-		if(rb.GetContacts(contacts)>1)
-		{
-			Debug.Log("time");
-		}
-
-		grounded = rb.GetContacts(contacts) != 0 ? true:false;
-        perp = new Vector2(contacts[0].normal.y, contacts[0].normal.x);
-
-        if(grounded){
-			Physics2D.gravity= new Vector2(0f,0f);
-        	rb.position += new Vector2(perp.x * Input.GetAxisRaw("Horizontal")*speed*Time.deltaTime, perp.y * -Input.GetAxisRaw("Horizontal")*speed* Time.deltaTime);
-		}
-		else{
-            Physics2D.gravity = new Vector2(0f, grav);
-
-            rb.position += new Vector2( Input.GetAxisRaw("Horizontal")*speed*Time.deltaTime,falling*Time.deltaTime);
-		}
-
-	
-			Debug.Log(Physics2D.gravity);
-		
-
-	}
-
-
-/*	public bool groundedState()
 	{
-		grounded = rb.GetContacts(contacts) != 0 ? true : false;
 
 		if(!grounded)
-			return false;
-        Vector2 perp = new Vector2(contacts[0].normal.y, contacts[0].normal.x);
-		float angle = Mathf.Rad2Deg * Mathf.Acos(perp.y / 1f);
+		{
+			Physics2D.gravity=new Vector2(0f,-9.81f);
+		}
+		else
+            Physics2D.gravity = new Vector2(0f, 0f);
+       
+	    Grounding();
+        float YMovement = grounded ? -Input.GetAxisRaw("Horizontal") * speed * perp.y : rb.velocity.y;
+		rb.velocity= new Vector2(Input.GetAxisRaw("Horizontal")*speed*perp.x,YMovement	);
 
-		if(<=angle)
 	}
-*/
+
+	public void Grounding()
+	{
+        RaycastHit2D FowardGroundCast = Physics2D.Linecast(transform.position+new Vector3(.5f,0,0), transform.position - new Vector3(-.5f, groundcheckVector, 0), groundLayer);
+        RaycastHit2D BackGroundCast = Physics2D.Linecast(transform.position-new Vector3(.5f, 0,0), transform.position - new Vector3(.5f, groundcheckVector, 0), groundLayer);
+
+        if (FowardGroundCast||BackGroundCast)
+        {
+
+            grounded = true;
+            jumping = false;
+        }
+        else
+        {
+            grounded = false;         
+        }
+
+        Color color = grounded ? Color.green : Color.red;
+        if (FowardGroundCast)
+            perp = new Vector2(FowardGroundCast.normal.y, FowardGroundCast.normal.x);
+        else if (BackGroundCast)
+            perp = new Vector2(BackGroundCast.normal.y, BackGroundCast.normal.x);
+
+
+         Debug.DrawLine(transform.position + new Vector3(.5f, 0, 0), transform.position - new Vector3(-.5f, groundcheckVector, 0), color);
+         Debug.DrawLine(transform.position - new Vector3(.5f, 0, 0), transform.position - new Vector3(.5f, groundcheckVector, 0), color);
+         
+
+
+		// teleport down check
+        Vector2 boxCastOrigin = new Vector2(transform.position.x + Input.GetAxisRaw("Horizontal"), transform.position.y);
+     	//   Debug.DrawRay(boxCastOrigin, Vector3.down, Color.blue);
+        if (!grounded && !jumping)
+        {
+            Debug.Log("lol");
+
+            RaycastHit2D SlopeleavingCast = Physics2D.BoxCast(boxCastOrigin, new Vector2(ColliderAttach.radius*2, ColliderAttach.radius*2), 0f, Vector2.down, TeleportCheckVector, groundLayer);
+            //   Debug.Log(SlopeleavingCast.distance + " distance");
+
+            if (SlopeleavingCast)
+            {
+                rb.position += new Vector2(0, -SlopeleavingCast.distance);
+            }
+            else
+            {
+                grounded = false;
+				jumping=true;
+				rb.velocity= new Vector2(0f,0f);
+
+            }
+
+
+             Color color2 = SlopeleavingCast ? Color.green : Color.red;
+             Debug.DrawLine(transform.position, transform.position - new Vector3(0, TeleportCheckVector, 0), color2);
+             
+			 
+        }
+      // Debug.Log(rb.velocity);
+
+
+    }
+
+
 
 }
