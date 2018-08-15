20c3957b5436f30650a01321c253e5104e440abd
Luis <lfluna@uci.edu>
2018-08-14 17:42:44 -0700

    Smooth Player Movement
    +character controller 2d

---
+++ VGDCPlatformer/Assets/scripts/Player/PlayerMove.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMove : MonoBehaviour {
+
+    [SerializeField] private float runSpeed;
+    float horizontalMove = 0f;
+    bool jump = false;
+    public CharacterController2D controller;
+
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        horizontalMove = Input.GetAxisRaw("Horizontal") * runSpeed;
+        if (Input.GetButtonDown("Jump"))
+        {
+            jump = true;
+        }
+    }
+
+    private void FixedUpdate()
+    {
+        controller.Move(horizontalMove * Time.fixedDeltaTime, jump);
+        jump = false;
+    }
+
+}
