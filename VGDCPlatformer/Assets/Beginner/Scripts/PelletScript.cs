74ec6decf215d1b4cf90f29995f6272a80670709
Joseph Riestra <jariestr@uci.edu>
2018-10-26 20:32:27 -0700

    Added Pellets and Shooting Scripts

---
+++ VGDCPlatformer/Assets/Beginner/Scripts/PelletScript.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PelletScript : MonoBehaviour {
+
+    public float pelletSpeed = 2f;
+
+    Rigidbody2D rb;
+
+	// Use this for initialization
+	void Start () {
+        rb = GetComponent<Rigidbody2D>();
+        rb.velocity = new Vector2(pelletSpeed , 0);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+	}
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        Debug.Log(collision.name);
+
+        Destroy(gameObject);
+    }
+}
