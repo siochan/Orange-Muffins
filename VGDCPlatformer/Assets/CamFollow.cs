276193a91eb0d18cb048ba395992abe78d7cf580
siochan <sallyhuynh@dhcp-10-8-035-139.mobile.reshsg.uci.edu>
2018-10-30 20:09:54 -0700

    ignore this

---
+++ VGDCPlatformer/Assets/CamFollow.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CamFollow : MonoBehaviour
+{
+
+    public Transform target;
+    public float smoothedSpeed = 0.1f;
+    Camera myCam;
+
+    void Start()
+    {
+        myCam = GetComponent<Camera>();
+    }
+    void FixedUpdate()
+    {
+        myCam.orthographicSize = (Screen.height / 100f) / 4f;
+        float interpolation = smoothedSpeed * Time.deltaTime;
+        if (target)
+        {
+            transform.position = Vector3.Lerp(transform.position, target.position, interpolation);
+        }
+        //transform.LookAt(target);
+    }
+}
---
+++ VGDCPlatformer/Assets/Intermediate/scripts/Enemies/ShootingEnemy.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShootingEnemy : MonoBehaviour {
+
+    public float speed = 10;
+    public float stoppingDistance = 20;
+    public float retreatDistance = 10;
+
+    private float timeBtShots;
+    private float startTimeBtShots;
+
+    public Transform FirePoint;
+    public GameObject projectile;
+    private Transform player;
+
+    void Start () {
+        player = GameObject.FindGameObjectWithTag("Player").transform;
+
+        timeBtShots = startTimeBtShots;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        /*
+        if (Vector2.Distance (transform.position, player.position) > stoppingDistance){
+            transform.position = Vector2.MoveTowards(transform.position, player.position, speed * Time.deltaTime); // enemy closes in
+        
+        } else if (Vector2.Distance(transform.position, player.position) < stoppingDistance && Vector2.Distance(transform.position, player.position) > retreatDistance){
+            transform.position = this.transform.position; // enemy does not move
+        
+        } else if (Vector2.Distance(transform.position, player.position) < retreatDistance){
+            transform.position = Vector2.MoveTowards(transform.position, player.position, -speed * Time.deltaTime); // enemy retreats
+        }
+        */
+        if (timeBtShots <= 0){
+            Instantiate(projectile, FirePoint.position, Quaternion.identity); //spawns projectile
+            timeBtShots = startTimeBtShots;
+        
+        } else {
+            timeBtShots -= Time.deltaTime;
+        }
+    }
+}
