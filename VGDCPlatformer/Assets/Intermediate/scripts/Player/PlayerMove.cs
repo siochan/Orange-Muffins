a70036d6d6512d3470f0b46294c6e9e2f652b359
Matthew Gonzales <matthekg@uci.edu>
2018-11-02 14:14:43 -0700

    Added a HUD for collected muffins
    Muffins!

--- VGDCPlatformer/Assets/Intermediate/scripts/Player/PlayerMove.cs
+++ VGDCPlatformer/Assets/Intermediate/scripts/Player/PlayerMove.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class PlayerMove : MonoBehaviour {
 
@@ -8,6 +9,10 @@ public class PlayerMove : MonoBehaviour {
     float horizontalMove = 0f;
     bool jump = false;
     public CharacterController2D controller;
+
+    [Header("Pickup HUD")]
+    public Text countText;
+    private int count;
 
     [Header("Shoot Logic")]
     // Shooting Logic
@@ -19,7 +24,8 @@ public class PlayerMove : MonoBehaviour {
 
     void Start()
     {
-
+        count = 0;
+        SetCountText();
     }
 
     // Update is called once per frame
@@ -51,7 +57,16 @@ public class PlayerMove : MonoBehaviour {
         if (other.gameObject.CompareTag("Pickup"))
         {
             other.gameObject.SetActive(false);
+            ++count;
+            SetCountText();
         }
+    }
+
+    //This function updates the text displaying the number of objects we've collected
+    void SetCountText()
+    {
+        //Set the text property of our our countText object to "Count: " followed by the number stored in our count variable.
+        countText.text = "Muffins: " + count.ToString();
     }
 
     void fire()
