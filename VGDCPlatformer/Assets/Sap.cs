570aa5f99f7f11acb8daf8541614f610bf663d57
Unknown <swelden@uci.edu>
2018-11-02 15:16:22 -0700

    Added sap to my script and tried making air jumps equal to 0 when enter sap.

--- VGDCPlatformer/Assets/Sap.cs
+++ VGDCPlatformer/Assets/Sap.cs
@@ -10,6 +10,10 @@ public class Sap : MonoBehaviour {
     private float heavy;
     private float previousRunSpeed;                         // store original run speed so that it can change back when exiting sap
     private float slowDown;                                 // will be used to get the desired reduced speed
+    private int previousAirJumps;
+    private int noAirJumps;
+
+    //private bool m_Grounded;
 
     private GameObject playerObj;
     PlayerMove player;
@@ -19,11 +23,14 @@ public class Sap : MonoBehaviour {
 	void Start () {
         playerObj = GameObject.Find("Player(Advance)");
         player = playerObj.GetComponent<PlayerMove>();
-        player2 = playerObj.GetComponent<CharacterController2D>();
         previousRunSpeed = player.runSpeed;
+        slowDown = previousRunSpeed - reducedSpeed;        // currentSpeed - slowDown = reducedSpeed => slowDown = previousSpeed - reduced speed
+        player2 = playerObj.GetComponent<CharacterController2D>();
         previousJumpPower = player2.m_JumpForce;
         heavy = previousJumpPower - reduceJumpPower;
-        slowDown = previousRunSpeed - reducedSpeed;        // currentSpeed - slowDown = reducedSpeed => slowDown = previousSpeed - reduced speed
+        previousAirJumps = player2.m_AirJumps;
+        noAirJumps = player2.m_AirJumps - 1;
+
 	}
 	
 	// Update is called once per frame
@@ -36,6 +43,7 @@ public class Sap : MonoBehaviour {
         if(collision.gameObject.tag == "Player")
             player.runSpeed -= slowDown;                // subtract orignal speed by slowDown to always obtain desired speed (reducedSpeed)
             player2.m_JumpForce -= heavy;
+            player2.m_AirJumps = noAirJumps;
     }
 
     private void OnTriggerExit2D(Collider2D collision)
@@ -43,5 +51,6 @@ public class Sap : MonoBehaviour {
         if (collision.gameObject.tag == "Player")
             player.runSpeed = previousRunSpeed;         // when player exits sap, return the run speed back to normal
             player2.m_JumpForce = previousJumpPower;
+            //player2.m_AirJumps = previousAirJumps;
     }
 }
