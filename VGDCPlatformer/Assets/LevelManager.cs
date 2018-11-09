3af1f8fecd282d8f564a257eef77448e22120913
Joseph Riestra <jariestr@uci.edu>
2018-11-09 13:59:10 -0800

    Delay before reload, Attach LevelManager script to GameManager object

---
+++ VGDCPlatformer/Assets/LevelManager.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LevelManager : MonoBehaviour {
+ 
+    //public GameObject playerObject;
+    public float sceneDelay = 2f;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	}
+
+    public void reloadLevel()
+    {
+        StartCoroutine(DelayReload(sceneDelay));
+    }
+
+    IEnumerator DelayReload(float delay)
+    {
+        yield return new WaitForSeconds(delay);
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+    }
+}
